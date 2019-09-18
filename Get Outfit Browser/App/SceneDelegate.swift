0f3297a954960d9971f0e73628373b9f4dc90674
dbystruev <github@learnswift.ru>
2019-09-18 19:21:54 +0300

    Re-arranged the files and set target to iOS 12.2

---
+++ Get Outfit Browser/App/SceneDelegate.swift
@@ -0,0 +1,14 @@
+//
+//  SceneDelegate.swift
+//  Get Outfit Browser
+//
+//  Created by Denis Bystruev on 18/09/2019.
+//  Copyright © 2019 Denis Bystruev. All rights reserved.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    var window: UIWindow?
+}
+
