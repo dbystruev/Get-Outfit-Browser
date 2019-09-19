cc72cb10ae561439b401b68e0b558a880aad03c6
dbystruev <github@learnswift.ru>
2019-09-19 21:48:24 +0300

    Implemented offer detail screen

--- Get Outfit Browser/View Controllers/OfferViewController.swift
+++ Get Outfit Browser/View Controllers/OfferViewController.swift
@@ -12,15 +12,19 @@ import UIKit
 class OfferViewController: UIViewController {
     
     @IBOutlet var imageView: UIImageView!
-    
+    @IBOutlet var categoryLabel: UILabel!
+    @IBOutlet var idLabel: UILabel!
+    @IBOutlet var vendorCodeLabel: UILabel!
+    @IBOutlet var descriptionLabel: UILabel!
+    @IBOutlet var salesNotesLabel: UILabel!
+    @IBOutlet var priceLabel: UILabel!
+
     var offer: YMLOffer?
 }
 
-// MARK: - UIViewController
+// MARK: - UI
 extension OfferViewController {
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
+    func updateUI() {
         let label = UILabel()
         label.adjustsFontSizeToFitWidth = true
         label.text = offer?.title
@@ -29,5 +33,61 @@ extension OfferViewController {
         if let imageURL = offer?.pictures.first {
             imageView?.sd_setImage(with: imageURL, placeholderImage: UIImage(named: "icons8-jumper"))
         }
+        
+        if let category = offer?.categoryId {
+            categoryLabel.text = "Category: \(category)"
+        } else {
+            categoryLabel.isHidden = true
+        }
+        
+        if let id = offer?.id {
+            idLabel.text = "id: \(id)"
+        } else {
+            idLabel.isHidden = true
+        }
+        
+        if let vendorCode = offer?.vendorCode {
+            vendorCodeLabel.text = "Vendor Code: \(vendorCode)"
+        } else {
+            vendorCodeLabel.isHidden = true
+        }
+        
+        if let description = offer?.description {
+            descriptionLabel.text = description
+        } else {
+            descriptionLabel.isHidden = true
+        }
+        
+        if let salesNotes = offer?.sales_notes {
+            salesNotesLabel.text = salesNotes
+        } else {
+            salesNotesLabel.isHidden = true
+        }
+
+        if let price = offer?.price {
+            priceLabel.text = "\(price) ₽"
+        } else {
+            priceLabel.isHidden = true
+        }
+    }
+    
+}
+
+// MARK: - UIViewController
+extension OfferViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        updateUI()
+    }
+}
+
+// MARK: - Actions
+extension OfferViewController {
+    @IBAction func imageTapped(_ sender: UITapGestureRecognizer) {
+        print(#line, #function)
+        guard let url = offer?.url else { return }
+        print(#line, #function, url)
+        
+        UIApplication.shared.open(url)
     }
 }
